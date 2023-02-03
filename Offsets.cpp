fe581ade83d917c27a655c48b9dc73b9d0a9c8d9
imyourfeel <79488366+imyourfeel@users.noreply.github.com>
2023-02-03 09:19:00 +0700

    Update Offsets.cpp

--- Offsets.cpp
+++ Offsets.cpp
@@ -1,20 +1,17 @@
-//season 15 offset update 10/01/23 https://pastebin.com/00GRLAtL
-
-#pragma once
 namespace offsets
 {
     // buttons
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    const long IN_ATTACK = 0x076665d8;              // [Buttons] -> in_attack
+    const long IN_ATTACK = 0x0766a958; // [Buttons] -> in_attack
     // core
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    const long REGION = 0x140000000;                // [Mine]          -> Region
-    const long LEVEL = 0x1449F00;                   // [Miscellaneous] -> LevelName
-    const long LOCAL_PLAYER = (0x1EDB670 + 0x8);    // [Miscellaneous] -> LocalPlayer
-    const long ENTITY_LIST = 0x1b2a578;             // [Miscellaneous] -> cl_entitylist
+    const long REGION = 0x140000000;     // [Mine]          -> Region
+    const long LEVEL = 0x144df40;        // [Miscellaneous] -> LevelName
+    const long LOCAL_PLAYER = (0x01edf800 + 0x8); // [Miscellaneous] -> LocalPlayer
+    const long ENTITY_LIST = 0x1b2e708;  // [Miscellaneous] -> cl_entitylist
     // entity
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    const long LOCAL_ORIGIN = 0x0158; 		    // [DataMap.CBaseViewModel]    -> m_localOrigin
+    const long LOCAL_ORIGIN = 0x0158; // [DataMap.CBaseViewModel]    -> m_localOrigin
     // player
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     const long GLOW_ENABLE = (0x03c0 + 0x8);        // [RecvTable.DT_HighlightSettings] -> m_highlightServerContextID + 0x8
@@ -26,5 +23,5 @@ namespace offsets
     const long VIEW_ANGLE = (0x25a4 - 0x14);        // [DataMap.C_Player]               -> m_ammoPoolCapacity - 0x14
     const long BLEEDOUT_STATE = 0x2738;             // [RecvTable.DT_Player]            -> m_bleedoutState
     const long ZOOMING = 0x1c51;                    // [RecvTable.DT_Player]            -> m_bZooming
-    const long LAST_VISIBLE_TIME = 0x1A70;          // [Miscellaneous]                  -> CPlayer!lastVisibleTime  1st offset from dump
+    const long LAST_VISIBLE_TIME = 0x1a70;          // [Miscellaneous]                  -> CPlayer!lastVisibleTime  1st offset from dump
 }
